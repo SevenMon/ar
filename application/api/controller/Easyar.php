bbb96e2b203e0fbf0504d96a883e39c1efaac031
SevenMon <57867782@qq.com>
2018-10-16 14:58:29 +0800

    no message

--- application/api/controller/Easyar.php
+++ application/api/controller/Easyar.php
@@ -62,7 +62,6 @@ class Easyar {
      */
     function getPostImage() {
         $data = @file_get_contents('php://input');
-        $this->base64_image_content($data,'.');
         if ($data) {
             $obj = json_decode($data);
             $data = $obj->image;
@@ -80,7 +79,6 @@ class Easyar {
             foreach ($_FILES as $file) {
                 if ($file['error'] == 0) {
                     $data = base64_encode(@file_get_contents($file['tmp_name']));
-                    $this->base64_image_content($data,'.');
                     break;
                 }
             }
@@ -88,29 +86,7 @@ class Easyar {
         return $data;
     }
 
-
-    function base64_image_content($base64_image_content,$path)
-    {
-        echo '123213';
-        exit();
-        //匹配出图片的格式
-        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image_content, $result)) {
-            $type = $result[2];
-            $new_file = $path . "/" . date('YmdHis', time()) . "/";
-            if (!file_exists($new_file)) {
-                //检查是否有该文件夹，如果没有就创建，并给予最高权限
-                mkdir($new_file, 0700);
-            }
-            $new_file = $new_file . time() . ".{$type}";
-            if (file_put_contents($new_file, base64_decode(str_replace($result[1], '', $base64_image_content)))) {
-                return '/' . $new_file;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+    
 
     /**
      * 生成签名，使用sha256加密
